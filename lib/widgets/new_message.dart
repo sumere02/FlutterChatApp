d03df9f73289ed5475ddd548071ac48f80ecf143
sumere02 <sumeremir@outlook.com>
2023-08-13 19:48:26 +0300

    Fluter chat app with firebase project files added.

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() {
+    return _NewMessageState();
+  }
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _messageController.dispose();
+  }
+
+  void _submitMessage() async {
+    final enteredMessage = _messageController.text;
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+    FocusScope.of(context).unfocus();
+    //Send to firebase
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    final currentUserData = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(currentUser.uid)
+        .get();
+
+    FirebaseFirestore.instance.collection("chat").add({
+      "text": _messageController.text,
+      "createdAt": Timestamp.now(),
+      "userId": currentUser.uid,
+      "username": currentUserData.data()!["username"],
+      "userImage": currentUserData.data()!["image_url"],
+    });
+    _messageController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(
+                labelText: "Send a message...",
+              ),
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            icon: const Icon(Icons.send),
+            onPressed: _submitMessage,
+          ),
+        ],
+      ),
+    );
+  }
+}
